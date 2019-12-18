0e638a1f36820fbd4c6c2594f4caee44b71872d4
Cherepakhin Vasili <vasi.che@gmail.com>
2019-12-18 17:43:53 +0500

    Реализован Service task в виде делегата, принимающего переменные.

---
+++ src/main/java/ru/perm/v/camunda/springbootfirst/ProcessRequestDelegate.java
@@ -0,0 +1,15 @@
+package ru.perm.v.camunda.springbootfirst;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProcessRequestDelegate implements JavaDelegate {
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(ProcessRequestDelegate.class);
+
+  public void execute(DelegateExecution execution) throws Exception {
+    LOGGER.info("Processing request by '" + execution.getVariable("customerId") + "'...");
+  }
+}
